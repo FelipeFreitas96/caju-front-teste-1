51daf1b3beaff3327a0324b6f36ef21c5c42ce2d
FelipeFreitas96 <5582601+FelipeFreitas96@users.noreply.github.com>
2024-09-12 10:47:41 -0300

    chore: adiciona env variables

--- src/queries/getRegistrationCards.ts
+++ src/queries/getRegistrationCards.ts
@@ -1,7 +1,7 @@
 import { RegistrationDTO } from "~/pages/Dashboard/interfaces";
 
 export async function getRegistrationCardsFn() {
-  const data = await fetch("http://localhost:3000/registrations");
+  const data = await fetch(`${import.meta.env.VITE_API_URL}/registrations`);
   const response = await data.json();
   return response as RegistrationDTO[];
 }
