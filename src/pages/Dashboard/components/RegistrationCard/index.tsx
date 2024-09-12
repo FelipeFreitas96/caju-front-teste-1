975c0dd300f90b735b8449998ec48dd9c8c9c3a1
FelipeFreitas96 <5582601+FelipeFreitas96@users.noreply.github.com>
2024-09-12 12:37:14 -0300

    feat: adiciona botões adicionar/reprovar em review

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -12,26 +12,32 @@ type Props = {
   data: RegistrationDTO;
 };
 
-const RegistrationCard = (props: Props) => {
+const RegistrationCard = ({ data }: Props) => {
+  const isInReview = data.status === "REVIEW";
   return (
     <S.Card>
       <S.IconAndText>
         <HiOutlineUser />
-        <h3>{props.data.employeeName}</h3>
+        <h3>{data.employeeName}</h3>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineMail />
-        <p>{props.data.email}</p>
+        <p>{data.email}</p>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineCalendar />
-        <span>{props.data.admissionDate}</span>
+        <span>{data.admissionDate}</span>
       </S.IconAndText>
       <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
-
+        {isInReview && (
+          <>
+            <ButtonSmall bgcolor="rgb(255, 145, 154)">Reprovar</ButtonSmall>
+            <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
+          </>
+        )}
+        {!isInReview && (
+          <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
+        )}
         <HiOutlineTrash />
       </S.Actions>
     </S.Card>
